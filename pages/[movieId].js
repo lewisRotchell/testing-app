008398f4707acb02497a2f63a237ffb5e9140b94
Lewis Rotchell <rotchell92@gmail.com>
2021-07-11 18:07:08 +0100

    testing

--- pages/[movieId].js
+++ pages/[movieId].js
@@ -32,7 +32,7 @@ export async function getStaticProps(context) {
   export async function getStaticPaths() {
 
 
-    const res = await getNewReleases()
+    const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-GB&page=1&region=GB`)
     const movies = await res.json() 
 
 
